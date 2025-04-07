c3c30d3d162f27aac91eb946f899e6f4f9968b65
robsonmuniz <robson_muniz@yahoo.com>
2025-04-07 13:07:18 +0100

    Building the App Layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import AppNav from "../components/AppNav.jsx";
+import AppNav from '../components/AppNav';
+import Sidebar from "../components/Sidebar.jsx";
+import Map from "../components/Map.jsx"; // Add Map import
+import styles from "../pages/AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App</p>
-    </div>
+   <div className={styles.app}>
+     <Sidebar />
+     <Map />
+   </div>
   );
 }
 
