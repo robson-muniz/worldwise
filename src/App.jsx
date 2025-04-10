ed165eb378359aae297c274269b08bdcb4711f24
robsonmuniz <robson_muniz@yahoo.com>
2025-04-10 11:56:54 +0100

    Implementing the Countries List

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
@@ -7,8 +8,9 @@ import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
+import CountryList from "./components/CountryList.jsx";
 
-const BASE_URL = "http://localhost:8000/"; // Keep trailing slash
+const BASE_URL = "http://localhost:3000/"; // Keep trailing slash
 
 function App() {
   const [cities, setCities] = useState([])
@@ -48,7 +50,7 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<p>List of Countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,38 @@
+// Import necessary dependencies
+import styles from "./CountryList.module.css"; // Component-specific CSS using CSS Modules
+import Spinner from "./Spinner.jsx"; // Loading spinner component
+import Message from "./Message.jsx";
+import CountryItem from "./CountryItem.jsx";
+import city from "./City.jsx"; // Reusable country item component
+
+// CityList component displays a list of cities with loading state handling
+// Props:
+// - cities: Array of country objects to display
+// - isLoading: Boolean indicating data loading status
+function CountryList({ cities, isLoading }) {
+  // Show loading spinner while data is being fetched
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message="No cities found." />
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.some(el => el.country === city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    }
+    return arr;
+  }, []);
+  // Main component render
+  return (
+    // Unordered list with styling from CSS Module
+    <ul className={styles.countryList}>
+      {/* Map through cities array to create CityItem components */}
+      {countries.map((country) => (
+        // Individual country item component
+        <CountryItem key={country.id} country={country} />
+
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
